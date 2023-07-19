9db43b779021246f3609cb68695a9d073dfb4c70
m-triple-m <triplem656@gmail.com>
2023-07-19 11:02:15 +0530

    js

---
+++ Section 1/index.js
@@ -0,0 +1,20 @@
+console.log('hello javascript');
+
+num = 35;
+
+console.log(num);
+
+message = "Nice";
+
+console.log(message);
+
+console.log( typeof num );
+console.log( typeof message );
+
+age = 30;
+
+if(age >= 18){
+    console.log('eligible for DL');
+}else{
+    console.log('not eligible for DL');
+}
