eb06f63d195ed5f26ecbbd3ae7aa158900e82632
yourusername <triplem656@gmail.com>
2023-08-05 10:36:40 +0530

    using map and conditional rendering

---
+++ Section 2/frontend/src/components/ProductList.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import smartphones from './dummyData';
+
+const ProductList = () => {
+
+    const [productData, setProductData] = useState(smartphones);
+
+    const displayProducts = () => {
+        return productData.map((phone) => {
+            return <div className='col-md-3'>
+                <div className='card'>
+                    <img className='card-img-top' src={phone.image} alt="" />
+                    <div className='card-body'>
+                        <h5>{phone.brand}</h5>
+                        <h3>{phone.model}</h3>
+                        <h2>₹{phone.price}</h2>
+                    </div>
+                </div>
+            </div>
+        })
+    }
+
+  return (
+    <div>
+        <div className='container'>
+            <div className='row'>
+                {displayProducts()}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductList;
