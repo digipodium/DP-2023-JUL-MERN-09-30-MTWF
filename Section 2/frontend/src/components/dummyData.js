4969897fae0e28593d05bc833b174033b639b5b4
yourusername <triplem656@gmail.com>
2023-08-06 10:28:12 +0530

    productlist

--- Section 2/frontend/src/components/dummyData.js
+++ Section 2/frontend/src/components/dummyData.js
@@ -6,20 +6,26 @@ const smartphones = [
         image : 'https://english.cdn.zeenews.com/sites/default/files/2022/12/24/1133231-untitled-design-2022-12-24t160052.453.jpg'
     },
     {
-        brand : 'Samsung',
-        model : 'Galaxy S23',
+        brand : 'Apple',
+        model : 'Iphone 14',
         price : 50000,
         image : 'https://english.cdn.zeenews.com/sites/default/files/2022/12/24/1133231-untitled-design-2022-12-24t160052.453.jpg'
     },
     {
-        brand : 'Samsung',
-        model : 'Galaxy S23',
+        brand : 'OnePlus',
+        model : 'OnePlus 11R',
         price : 80000,
         image : 'https://english.cdn.zeenews.com/sites/default/files/2022/12/24/1133231-untitled-design-2022-12-24t160052.453.jpg'
     },
     {
         brand : 'Samsung',
-        model : 'Galaxy S23',
+        model : 'A20',
+        price : 20000,
+        image : 'https://english.cdn.zeenews.com/sites/default/files/2022/12/24/1133231-untitled-design-2022-12-24t160052.453.jpg'
+    },
+    {
+        brand : 'Moto',
+        model : 'Razor 14',
         price : 80000,
         image : 'https://english.cdn.zeenews.com/sites/default/files/2022/12/24/1133231-untitled-design-2022-12-24t160052.453.jpg'
     },
