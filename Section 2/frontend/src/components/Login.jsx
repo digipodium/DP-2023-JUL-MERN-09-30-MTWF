703ad8e9786f8e0959866b2f1f4c363257960bd2
yourusername <triplem656@gmail.com>
2023-08-07 10:43:32 +0530

    added login form

--- Section 2/frontend/src/components/Login.jsx
+++ Section 2/frontend/src/components/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <div className="w-25">
+        <div className="card">
+          <div className="card-body">
+            <h3 className="text-center">Login Form</h3>
+            <hr />
+
+            <form>
+              <label htmlFor="">Email Address</label>
+              <input type="email" className="form-control mb-3" />
+
+              <label htmlFor="">Password</label>
+              <input type="password" className="form-control mb-3" />
+
+              <button className="btn btn-primary w-100 mt-5">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
