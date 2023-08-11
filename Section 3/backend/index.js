01bc9a685fb6937c816e527e04e59b38dbcc3877
yourusername <triplem656@gmail.com>
2023-08-11 11:03:43 +0530

    express intro

--- Section 3/backend/index.js
+++ Section 3/backend/index.js
@@ -1,9 +1,35 @@
 // import express
-const express = require('express');
+const express = require("express");
+
+// importing routers
+const userRouter = require('./routers/userRouter');
+const blogRouter = require('./routers/blogRouter');
 
 // initialize express app
 const app = express();
 const port = 5000;
 
+// middleware
+app.use('/user', userRouter);
+app.use('/blog', blogRouter);
 
-app.listen( port, () => { console.log('server started') } );
+// routes
+app.get("/", (req, res) => {
+  res.send("Response from express");
+});
+
+app.get("/add", (req, res) => {
+  res.send("Response from add");
+});
+
+app.get("/getall", (req, res) => {
+  res.send("Response from getall");
+});
+
+app.get("/delete", (req, res) => {
+  res.send("Response from delete");
+});
+
+app.listen(port, () => {
+  console.log("express server started at 5000");
+});
