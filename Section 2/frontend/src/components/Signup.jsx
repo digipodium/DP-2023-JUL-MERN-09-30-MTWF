31c9a3db16009d76f0aa9df7ceb474462b2df6e0
yourusername <triplem656@gmail.com>
2023-08-17 09:58:34 +0530

    signup

--- Section 2/frontend/src/components/Signup.jsx
+++ Section 2/frontend/src/components/Signup.jsx
@@ -1,11 +1,53 @@
+import { useFormik } from 'formik';
 import React from 'react'
 
 const Signup = () => {
+
+  const signupForm = useFormik({
+    initialValues: {
+      name : "",
+      email : "",
+      password : "",
+      age : ""
+    },
+    onSubmit : ( values ) => {
+      console.log(values);
+      // write code to submit form to server
+    }
+  });
+
   return (
     <div>
-        <h1>Signup Page</h1>
+      <div className="w-25">
+        <div className="card">
+          <div className="card-body">
+            <h3 className="text-center">Login Form</h3>
+            <hr />
+
+            <form onSubmit={signupForm.handleSubmit}>
+              <label htmlFor="">Name</label>
+              <span style={{color: 'red', fontSize: '0.7em', marginLeft: 10}}>{signupForm.errors.name}</span>
+              <input type="text" className="form-control mb-3" name="name" onChange={signupForm.handleChange} value={signupForm.values.name} />
+              
+              <label htmlFor="">Email Address</label>
+              <span style={{color: 'red', fontSize: '0.7em', marginLeft: 10}}>{signupForm.errors.email}</span>
+              <input type="email" className="form-control mb-3" name="email" onChange={signupForm.handleChange} value={signupForm.values.email} />
+
+              <label htmlFor="">Password</label>
+              <span style={{color: 'red', fontSize: '0.7em', marginLeft: 10}}>{signupForm.errors.password}</span>
+              <input type="password" className="form-control mb-3" name="password" onChange={signupForm.handleChange} value={signupForm.values.password} />
+              
+              <label htmlFor="">Age</label>
+              <span style={{color: 'red', fontSize: '0.7em', marginLeft: 10}}>{signupForm.errors.age}</span>
+              <input type="number" className="form-control mb-3" name="age" onChange={signupForm.handleChange} value={signupForm.values.age} />
+
+              <button className="btn btn-primary w-100 mt-5">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Signup;
